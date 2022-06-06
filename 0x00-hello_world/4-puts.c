59575b493f8fde209725657b7f7d9027038a5f29
Ethan-Amos <ethandoirramos@gmail.com>
2022-06-06 06:39:54 -0700

    First commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(void);
+int main(void)
 {
 	char code[] = "Programming is like building a multilingual puzzle";
 		puts(code);
